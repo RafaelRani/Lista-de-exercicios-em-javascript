dc08aac51becbd90c144ce5f6781b31e174ff953
Rafael Rani Santos Martins <ranny008@hotmail.com>
2020-04-07 03:11:44 -0300

    updates

--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
 const express = require('express')
-const questionsController = require('./questionsController')
+const questions = require('./questions')
 
 const routes = express.Router()
 //lista de exercícios:
 //https://www.inf.pucrs.br/~pinho/LaproI/Exercicios/SeqDecisao/lista1.htm
 
-routes.get('/questao1', questionsController.questao1)
-routes.get('/questao2', questionsController.questao2)
-routes.get('/questao3', questionsController.questao3)
-routes.get('/questao4', questionsController.questao4)
-routes.get('/questao5', questionsController.questao5)
-routes.get('/questao6', questionsController.questao6)
-routes.get('/questao7', questionsController.questao7)
-routes.get('/questao8', questionsController.questao8)
+routes.get('/questao1', questions.questao1)
+routes.get('/questao2', questions.questao2)
+routes.get('/questao3', questions.questao3)
+routes.get('/questao4', questions.questao4)
+routes.get('/questao5', questions.questao5)
+routes.get('/questao6', questions.questao6)
+routes.get('/questao7', questions.questao7)
+routes.get('/questao8', questions.questao8)
 
 module.exports = routes
